981fc5f0d864cbe044673c599c7d42ff30b03b28
Manudo Killer <una.esteban@gmail.com>
2021-04-27 02:45:00 -0600

    Access to Urls without login v2.0

--- front-end/src/components/Form.js
+++ front-end/src/components/Form.js
@@ -1,8 +1,33 @@
-import React, {useState} from 'react';
+import React from 'react';
+import UrlTable from './UrlTable';
 
-const Form = () => {
+function Form() {
+
+  var form = {};
+  const url = "http://localhost:3001/add_url";
+
+  const addUrl = () => {
+    fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(form)
+    });
+    UrlTable.getUrls();
+  }
+
+  const handleChange = async e=>{
+    e.persist();
+    form = {url: e.target.value};
+  }
+
   return(
-    
+    <form>
+      <div className="form-group">
+        <label htmlFor="url"><i className="fas fa-link"></i> - Url</label>
+        <input type="text" name="url" className="form-control" id="url" onChange={handleChange} />
+      </div>
+      <button type="submit" className="btn btn-success" onClick={() => addUrl()}>Save</button>
+    </form>
   );
 }
 
--- front-end/src/components/UrlTable.js
+++ front-end/src/components/UrlTable.js
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-const UrlTable = (props) => {
+function UrlTable() {
+
+  useEffect(() => {
+    getUrls();
+  }, []);
+
+  const [urls, setUrls] = useState([]);
+  
+  const get_url = "http://localhost:3001/";
+
+  const getUrls = async () => {
+    const data = await fetch(get_url);
+    setUrls(await data.json());
+  };
+
+  const getUrl = async (short_url) => {
+    await fetch(get_url + short_url);
+    getUrls();
+  };
+
   return(
-    
+    <table className="table table-hover">
+      <thead>
+        <tr>
+          <th>Short Url</th>
+          <th>Requests</th>
+          <th>Go There</th>
+        </tr>
+      </thead>
+      <tbody>
+        {urls.map(url => (
+          <tr key={url._id}>
+            <td>{url.short_url}</td>
+            <td>{url.count}</td>
+            <td>
+              <a href={url.url} className="btn btn-info" onClick={() => getUrl(url.short_url)} target="_blank" rel="noreferrer">
+                <i className="fas fa-link"></i>
+              </a>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
